49d7db32c43d1a04c596cbbb282a9521be1e7fc8
Valerio <velardovalerio@gmail.com>
2021-03-29 11:43:31 +0200

    Added VAE implementation for video 11

---
+++ 11 Implementing VAE/code/analysis.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from autoencoder import VAE
+from train import load_mnist
+
+
+def select_images(images, labels, num_images=10):
+    sample_images_index = np.random.choice(range(len(images)), num_images)
+    sample_images = images[sample_images_index]
+    sample_labels = labels[sample_images_index]
+    return sample_images, sample_labels
+
+
+def plot_reconstructed_images(images, reconstructed_images):
+    fig = plt.figure(figsize=(15, 3))
+    num_images = len(images)
+    for i, (image, reconstructed_image) in enumerate(zip(images, reconstructed_images)):
+        image = image.squeeze()
+        ax = fig.add_subplot(2, num_images, i + 1)
+        ax.axis("off")
+        ax.imshow(image, cmap="gray_r")
+        reconstructed_image = reconstructed_image.squeeze()
+        ax = fig.add_subplot(2, num_images, i + num_images + 1)
+        ax.axis("off")
+        ax.imshow(reconstructed_image, cmap="gray_r")
+    plt.show()
+
+
+def plot_images_encoded_in_latent_space(latent_representations, sample_labels):
+    plt.figure(figsize=(10, 10))
+    plt.scatter(latent_representations[:, 0],
+                latent_representations[:, 1],
+                cmap="rainbow",
+                c=sample_labels,
+                alpha=0.5,
+                s=2)
+    plt.colorbar()
+    plt.show()
+
+
+if __name__ == "__main__":
+    autoencoder = VAE.load("model")
+    x_train, y_train, x_test, y_test = load_mnist()
+
+    num_sample_images_to_show = 8
+    sample_images, _ = select_images(x_test, y_test, num_sample_images_to_show)
+    reconstructed_images, _ = autoencoder.reconstruct(sample_images)
+    plot_reconstructed_images(sample_images, reconstructed_images)
+
+    num_images = 6000
+    sample_images, sample_labels = select_images(x_test, y_test, num_images)
+    _, latent_representations = autoencoder.reconstruct(sample_images)
+    plot_images_encoded_in_latent_space(latent_representations, sample_labels)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
